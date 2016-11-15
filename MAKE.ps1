d6ce966e17fbe92e8024d11133b1475853034c10
emkael <emkael@tlen.pl>
2016-11-15 02:27:08 +0100

    * separate dist paths for both builds

--- MAKE.ps1
+++ MAKE.ps1
@@ -1,9 +1,9 @@
-& pyinstaller bidding_data.spec
-Copy-Item '*.md' -Destination 'dist' -Force
-Copy-Item 'res\*' -Destination 'dist' -Force -Recurse
-Set-Variable -Name VersionInfo -Value (Get-Item 'dist\bidding_data.exe').VersionInfo
+& pyinstaller bidding_data.spec --distpath dist\console
+Copy-Item '*.md' -Destination 'dist\console' -Force
+Copy-Item 'res\*' -Destination 'dist\console' -Force -Recurse
+Set-Variable -Name VersionInfo -Value (Get-Item 'dist\console\bidding_data.exe').VersionInfo
 Set-Variable -Name FileVersion -Value $VersionInfo.FileVersion.Split(',')
 Set-Variable -Name BundleName -Value ('bundle\\' + $VersionInfo.InternalName + '-' + $FileVersion[0].Trim() + '.' + $FileVersion[1].Trim() + '.zip')
 Remove-Item $BundleName -ErrorAction SilentlyContinue
 Add-Type -Assembly 'System.IO.Compression.FileSystem'
-[System.IO.Compression.ZipFile]::CreateFromDirectory('dist', $BundleName)
+[System.IO.Compression.ZipFile]::CreateFromDirectory('dist\console', $BundleName)
--- MAKE_GUI.ps1
+++ MAKE_GUI.ps1
@@ -1,9 +1,9 @@
-& pyinstaller bidding_data_gui.spec
-Copy-Item '*.md' -Destination 'dist' -Force
-Copy-Item 'res\*' -Destination 'dist' -Force -Recurse
-Set-Variable -Name VersionInfo -Value (Get-Item 'dist\bidding_data.exe').VersionInfo
+& pyinstaller bidding_data_gui.spec --distpath dist\gui
+Copy-Item '*.md' -Destination 'dist\gui' -Force
+Copy-Item 'res\*' -Destination 'dist\gui' -Force -Recurse
+Set-Variable -Name VersionInfo -Value (Get-Item 'dist\gui\bidding_data.exe').VersionInfo
 Set-Variable -Name FileVersion -Value $VersionInfo.FileVersion.Split(',')
 Set-Variable -Name BundleName -Value ('bundle\\' + $VersionInfo.InternalName + '-' + $FileVersion[0].Trim() + '.' + $FileVersion[1].Trim() + '-gui.zip')
 Remove-Item $BundleName -ErrorAction SilentlyContinue
 Add-Type -Assembly 'System.IO.Compression.FileSystem'
-[System.IO.Compression.ZipFile]::CreateFromDirectory('dist', $BundleName)
+[System.IO.Compression.ZipFile]::CreateFromDirectory('dist\gui', $BundleName)
