1bda1e000faf1e10cf0cb2860b8d07a1edd88c0b
emkael <emkael@tlen.pl>
2017-12-12 13:23:40 +0100

    Displaying bidding tables in the original table, not in a pop-over

--- res/javas/bidding.js
+++ res/javas/bidding.js
@@ -1,40 +1,36 @@
 var display_bidding = function(element, bidding) {
-    var popup = $('<div id="bidding_popup"></div>');
-    popup.css({
-        'position': 'absolute',
-        'width': '250px',
-        'left': element.offset().left + element.width(),
-        'top': element.offset().top
-    });
-    popup.html(bidding);
-    $('body').append(popup);
+    var popup = $('<tr class="bidding_popup"><td class="n">&nbsp;</td><td class="bidding_cell noc" colspan="1000"></td></tr>');
+    popup.find('.bidding_cell').html(bidding);
+    element.closest('tr').after(popup);
+    element.data('bidding-row', popup);
 }
 
 var load_bidding = function() {
-    $('#bidding_popup').remove();
     var elem = $(this);
-    $.ajax(
-        {
-            url: elem.attr('data-bidding-link'),
-            complete: function(xhr, status) {
-                if (status == 'success') {
-                    display_bidding(elem, xhr.responseText);
-                }
-                else {
-                    display_bidding(elem, 'Brak danych');
+    if (elem.data('bidding-row')) {
+        elem.data('bidding-row').remove();
+        elem.removeData('bidding-row');
+    } else {
+        $.ajax(
+            {
+                url: elem.attr('data-bidding-link'),
+                complete: function(xhr, status) {
+                    if (status == 'success') {
+                        display_bidding(elem, xhr.responseText);
+                    }
+                    else {
+                        display_bidding(elem, 'Brak danych');
+                    }
                 }
             }
-        }
-    );
+        );
+    }
     return false;
 };
 
 var bind_bidding_links = function() {
     $('a.biddingLink').each(function() {
-        $(this).unbind('click').click(load_bidding);
-    });
-    $(document).click(function() {
-        $('#bidding_popup').remove();
+        $(this).unbind('click').attr('title', 'Pokaż/ukryj licytację').click(load_bidding);
     });
 };
 
